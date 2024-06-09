2671d83faf1ac3a7d3d76123697e2b56b2d1ecfe
LGyuWon <152961797+LGyuWon@users.noreply.github.com>
2024-06-09 17:31:23 +0900

    Add files via upload

---
+++ ForPortfolio/BoradProject/src/main/java/portfolio/boradproject/BoradProjectApplication.java
@@ -0,0 +1,13 @@
+package portfolio.boradproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoradProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoradProjectApplication.class, args);
+    }
+
+}
